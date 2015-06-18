97ff1450d2026bbea441625c85453e2e27111254
Connor <connorbfranklin@gmail.com>
2015-06-18 12:28:18 -0400

    rendering mustache template

--- server.js
+++ server.js
@@ -7,22 +7,16 @@ var apiUrl = 'http://m.gsa.gov/api';
 app.use('/api', function(req, res) {
     var url = apiUrl + req.url;
     console.log('request: ', url)
-    /*req.pipe(request({
-    	headers: {
-    		'Accept': 'application/json'
-    	},
-    	uri: url
-    })).pipe(res);*/
-request({
-    	headers: {
-    		'Accept': 'application/json'
-    	},
-    	uri: url
-    },function(error,response,json){
-    	json = JSON.parse(json)
-    	res.header('Access-Control-Allow-Origin','*').json(json);
+    request({
+    	uri: url,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }, function(error, response, json) {
+        json = JSON.parse(json)
+        res.header('Access-Control-Allow-Origin', '*').json(json);
     })
-	
+
 });
 
 app.listen(process.env.PORT || 3000);
